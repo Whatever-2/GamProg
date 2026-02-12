821a58e3c484ac8cb0b4cf60d0f50bf9b48382d1
Alreim <57833694+Whatever-2@users.noreply.github.com>
2026-02-12 19:49:31 +0800

    almost done with quest 4

--- Assets/Scripts/Q4/SceneManager.cs
+++ Assets/Scripts/Q4/SceneManager.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public void LoadScene(string sceneName)
     {
-        
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
+
+    public void RestartScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadNextSceneInBuild()
+    {
+        int currentSceneIndex = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.Log("No more scenes to load.");
+        }
+    }
+
+
 }
